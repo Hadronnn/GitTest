19c46318e77c54eb322f25b3962651b64faaec3a
hadronnn <1364636420@qq.com>
2019-07-04 19:52:58 +0800

    GitHubTest 的 test2

--- src/Test2.java
+++ src/Test2.java
@@ -2,5 +2,8 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Test2");
+        System.out.println("Test3");
+        System.out.println("Test23");
+
     }
 }
